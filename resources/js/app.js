b22543309a5caf67e935236e6543f570336c8a64
Arentus <robwert7991@gmail.com>
2019-08-22 17:45:59 -0400

    fixed vuetify navbar styles

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 import './bootstrap';
 import Vue from 'vue';
+import Vuef from 'vuetify';
 import vuetify from './plugins/vuetify';
 
 // Route information for Vue Router
@@ -7,6 +8,8 @@ import Routes from '@/js/routes.js';
 
 // Component File
 import App from '@/js/views/App';
+
+Vue.use(Vuef)
 
 const app = new Vue({
 	el: '#app',
--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,15 +1,57 @@
 <template>
 	<v-app>
-		<v-navigation-drawer>
+		<v-navigation-drawer app>
 		</v-navigation-drawer>
+		 <v-app-bar
+      color="deep-purple accent-4"
+      dark
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-		<v-toolbar>
+      <v-toolbar-title>Page title</v-toolbar-title>
+
+      <div class="flex-grow-1"></div>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-menu
+        left
+        bottom
+      >
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+            v-for="n in 5"
+            :key="n"
+            @click="() => {}"
+          >
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+		<v-app-bar>
+			
+			<router-link tag="li" to="/">Home</router-link>
+			<router-link tag="li" to="/about">About</router-link>
+
+		</v-app-bar>
+		<v-toolbar app>
 		</v-toolbar>
 
 		<v-content step="1">
 		  <v-container fluid>
-			<router-link tag="li" to="/">Home</router-link>
-			<router-link tag="li" to="/about">About</router-link>
 
 			<!-- the most important shit-->
 
@@ -17,7 +59,7 @@
 		  </v-container>
 		</v-content>
 
-		<v-footer>
+		<v-footer app>
 		</v-footer>
 	</v-app>
 </template>
