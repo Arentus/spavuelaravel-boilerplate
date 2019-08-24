b832f3a27a55544ee24bf95e1473eb5b17e77eef
Arentus <robwert7991@gmail.com>
2019-08-24 15:05:26 -0400

    fixed nav

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,52 +1,53 @@
 <template>
 	<v-app>
-		<v-navigation-drawer app>
-		</v-navigation-drawer>
-		 <v-app-bar
-      color="deep-purple accent-4"
-      dark
-    >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+		<v-navigation-drawer app></v-navigation-drawer>
+			 
+		  <v-app-bar dark>
+	      
+	      <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Page title</v-toolbar-title>
+	      <v-toolbar-title>CANTV</v-toolbar-title>
 
-      <div class="flex-grow-1"></div>
+	      <div class="flex-grow-1"></div>
 
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
+	      <v-btn icon>
+	        <v-icon>mdi-heart</v-icon>
+	      </v-btn>
 
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-menu
-        left
-        bottom
-      >
-        <template v-slot:activator="{ on }">
-          <v-btn icon v-on="on">
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </template>
-
-        <v-list>
-          <v-list-item
-            v-for="n in 5"
-            :key="n"
-            @click="() => {}"
-          >
-            <v-list-item-title>Option {{ n }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-    </v-app-bar>
-		<v-app-bar>
-			
+	      <v-btn >
+	        <v-icon>mdi-magnify</v-icon>
+	      </v-btn>
+	      <v-btn >
 			<router-link tag="li" to="/">Home</router-link>
-			<router-link tag="li" to="/about">About</router-link>
+	      </v-btn>
 
-		</v-app-bar>
+
+	      <v-btn icon>
+			<router-link tag="li" to="/about">About</router-link>
+	      </v-btn>
+	      <v-menu
+	        left
+	        bottom
+	      >
+	        <template v-slot:activator="{ on }">
+	          <v-btn icon v-on="on">
+	            <v-icon>mdi-dots-vertical</v-icon>
+	          </v-btn>
+	        </template>
+
+	        <v-list>
+	          <v-list-item
+	            v-for="n in 5"
+	            :key="n"
+	            @click="() => {}"
+	          >
+	            <v-list-item-title>Option {{ n }}</v-list-item-title>
+	          </v-list-item>
+	        </v-list>
+	      </v-menu>
+	    </v-app-bar>
+
+	    
 		<v-toolbar app>
 		</v-toolbar>
 
